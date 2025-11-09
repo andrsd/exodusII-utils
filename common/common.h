e388da6665129cedbaa84dbc66caa3112f1efb52
David Andrs <andrsd@gmail.com>
2025-11-09 15:04:04 -0700

    Adding exo-info

---
+++ common/common.h
@@ -0,0 +1,73 @@
+// SPDX-FileCopyrightText: 2025 (c) David Andrs <andrsd@gmail.com>
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+#include <fmt/core.h>
+#include <string>
+#include <algorithm>
+
+enum class ElementType {
+    //
+    POINT1,
+    SEGMENT2,
+    TRI3,
+    QUAD4,
+    TET4,
+    HEX8,
+    PRISM6,
+    PYRAMID5
+};
+
+/// Convert string representation of an element type into enum
+inline ElementType
+element_type(std::string_view str)
+{
+    if (str == "BAR2")
+        return ElementType::SEGMENT2;
+    else if (str == "TRI" || str == "TRI3")
+        return ElementType::TRI3;
+    else if (str == "QUAD" || str == "QUAD4")
+        return ElementType::QUAD4;
+    else
+        throw std::runtime_error(fmt::format("Unsupported element type {}", str));
+}
+
+inline const char *
+element_type_str(ElementType et)
+{
+    if (et == ElementType::SEGMENT2)
+        return "BAR2";
+    else if (et == ElementType::TRI3)
+        return "TRI3";
+    else if (et == ElementType::QUAD4)
+        return "QUAD4";
+    else if (et == ElementType::TET4)
+        return "TET4";
+    else
+        throw std::runtime_error(fmt::format("Unsupported element type"));
+}
+
+/// Print number in human readable format
+///
+/// @param value Value to conert into humna-readable string
+/// @return Human-readable string
+template <typename T>
+inline std::string
+human_number(T value)
+{
+    std::string num_str = std::to_string(value);
+    std::string result;
+
+    int count = 0;
+    for (auto it = num_str.rbegin(); it != num_str.rend(); ++it) {
+        if (count && count % 3 == 0) {
+            result += ',';
+        }
+        result += *it;
+        ++count;
+    }
+
+    std::reverse(result.begin(), result.end());
+    return result;
+}
---
+++ exo-info/main.cpp
@@ -0,0 +1,88 @@
+// SPDX-FileCopyrightText: 2025 David Andrs <andrsd@gmail.com>
+// SPDX-License-Identifier: MIT
+
+#include "common.h"
+#include "cxxopts/cxxopts.hpp"
+#include <exodusIIcpp/exodusIIcpp.h>
+#include <fmt/core.h>
+
+void
+print_cell_set_info(const exodusIIcpp::File & exo)
+{
+    const auto & blocks = exo.get_element_blocks();
+    if (blocks.size() > 0) {
+        fmt::print("\n");
+        fmt::print("Cell sets [{}]:\n", blocks.size());
+        std::size_t wd_id = 1;
+        std::size_t wd_name = 1;
+        std::size_t wd_num = 1;
+        for (const auto & eb : blocks) {
+            wd_id = std::max(wd_id, fmt::format("{}", eb.get_id()).size());
+            auto name = eb.get_name();
+            if (name.size() == 0)
+                name = "<no name>";
+            wd_name = std::max(wd_name, fmt::format("{}", name).size());
+            wd_num =
+                std::max(wd_num, fmt::format("{}", human_number(eb.get_num_elements())).size());
+        }
+        wd_name++;
+
+        for (const auto & eb : blocks) {
+            // auto elem_counts = element_counts(mesh, id);
+            auto id = eb.get_id();
+
+            fmt::print("- {:>{}}: ", id, wd_id);
+
+            auto name = eb.get_name();
+            if (name.size() == 0)
+                name = "<no name>";
+            fmt::print("{:<{}} ", name, wd_name);
+
+            fmt::print("{:>{}} elements ", human_number(eb.get_num_elements()), wd_num);
+            fmt::print(" (");
+            auto etyp = element_type(eb.get_element_type());
+            fmt::print("{}", element_type_str(etyp));
+            fmt::print(")");
+            fmt::print("\n");
+        }
+    }
+}
+
+void
+print_mesh_info(const std::string & filename)
+{
+    fmt::print("Reading file: {}...", filename);
+    std::fflush(stdout);
+    exodusIIcpp::File exo(filename, exodusIIcpp::FileAccess::READ);
+    exo.read_blocks();
+    fmt::print(" done\n");
+
+    fmt::print("\n");
+    fmt::print("Global:\n");
+    fmt::print("- {} elements\n", human_number(exo.get_num_elements()));
+    fmt::print("- {} nodes\n", human_number(exo.get_num_nodes()));
+
+    print_cell_set_info(exo);
+    // print_side_set_info(mesh);
+}
+
+int
+main(int argc, char * argv[])
+{
+    cxxopts::Options options("exo-info", "Display information about an exodusII file");
+    // clang-format off
+    options.add_options()
+        ("filename", "The mesh file name", cxxopts::value<std::string>())
+        ("h,help", "Print usage")
+    ;
+    // clang-format on
+    options.parse_positional({ "filename" });
+
+    auto result = options.parse(argc, argv);
+    if (result["filename"].count())
+        print_mesh_info(result["filename"].as<std::string>());
+    else {
+        fmt::print("{}\n", options.help());
+    }
+    return 0;
+}
